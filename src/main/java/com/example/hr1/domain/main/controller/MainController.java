aae3b5febe2f0922e86829875a73c36a8c626e72
ysh <dbtmdgks7897@outlook.kr>
2023-07-17 13:12:54 +0900

    main 연결

--- src/main/java/com/example/hr1/domain/main/controller/MainController.java
+++ src/main/java/com/example/hr1/domain/main/controller/MainController.java
@@ -1,5 +1,20 @@
 package com.example.hr1.domain.main.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class MainController {
+ 
+    @GetMapping("/")
+    // Mapping을 기반으로 함수를 실행 시키기 때문에
+    // controller의 함수 이름은 중요하지 않다
+    //
+    public ModelAndView mainPage(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("main/main"); 
+        return modelAndView;
     
+    }
 }
