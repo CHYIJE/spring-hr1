35f1ce02c425c89eccdf82f0f4ca9d14a42ab4a4
ysh <dbtmdgks7897@outlook.kr>
2023-07-18 17:02:54 +0900

    detail연결

--- src/main/java/com/example/hr1/domain/emp/controller/EmpController.java
+++ src/main/java/com/example/hr1/domain/emp/controller/EmpController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.hr1.domain.emp.dto.EmpDetailDTO;
 import com.example.hr1.domain.emp.dto.EmpTableDTO;
 import com.example.hr1.domain.emp.service.EmpService;
 
@@ -25,9 +27,14 @@ public class EmpController {
         return modelAndView;
     }
 
-    @GetMapping("/emp/1")
-    public ModelAndView getEmpDetailPage(){
+    @GetMapping("/emp/{employeeId}")
+    public ModelAndView getEmpDetailPage(@PathVariable Integer employeeId){
+        System.out.println(employeeId);
+
         ModelAndView modelAndView = new ModelAndView();
+
+        EmpDetailDTO empDetailDTO = empService.getEmpDetailData(employeeId);
+        modelAndView.addObject("empDetailDTO", empDetailDTO);
         modelAndView.setViewName("emp/emp-detail");
         return modelAndView;
     }
--- src/main/java/com/example/hr1/model/employees/repository/EmployeesRepository.java
+++ src/main/java/com/example/hr1/model/employees/repository/EmployeesRepository.java
@@ -3,7 +3,10 @@ package com.example.hr1.model.employees.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.hr1.model.employees.entity.EmployeesEntity;
+import java.util.Optional;
+
 
 public interface EmployeesRepository extends JpaRepository<EmployeesEntity, Integer> {
+    Optional<EmployeesEntity> findByEmployeeId(Integer employeeId);
     
 }
