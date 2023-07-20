ab953f1aaadcb7d499c6885c0a9d580b71488715
ysh <dbtmdgks7897@outlook.kr>
2023-07-20 15:43:40 +0900

    restAPI

--- src/main/java/com/example/hr1/domain/main/controller/MainControllerApiV1.java
+++ src/main/java/com/example/hr1/domain/main/controller/MainControllerApiV1.java
@@ -6,19 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.hr1.domain.main.dto.ResMainDTO;
 import com.example.hr1.domain.main.service.MainService;
 
 
-@Controller
+@RestController
 public class MainControllerApiV1 {
 
     @Autowired
     private MainService mainService;
     
     @GetMapping("/api/v1/main")
-    public @ResponseBody List<ResMainDTO> getMainData(){
+    public List<ResMainDTO> getMainData(){
+        return mainService.getMainPageData();
+
+    }
+
+    @GetMapping("/api/v2/main")
+    public List<ResMainDTO> getMainData2(){
         return mainService.getMainPageData();
 
     }
